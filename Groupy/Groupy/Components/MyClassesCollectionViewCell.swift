a2f12cf2dccd411079d49c5b99819678251dd36f
Clissia Bovi <bovibc@gmail.com>
2023-07-04 11:19:59 -0300

    my classes cell

---
+++ Groupy/Groupy/Components/MyClassesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyClassesCollectionViewCell.swift
+//  Groupy
+//
+//  Created by Clissia Bozzer Bovi on 04/07/23.
+//
+
+import UIKit
+
+class MyClassesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var firstProject: UILabel!
+    @IBOutlet weak var secondProject: UILabel!
+    
+    
+}
--- Groupy/Groupy/Main/MyClassesViewController.swift
+++ Groupy/Groupy/Main/MyClassesViewController.swift
@@ -7,19 +7,41 @@
 
 import UIKit
 
-class MyClassesViewController: UIViewController {
+class MyClassesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureItem()
+        setCollectionView()
+        self.configureNavigationBar()
     }
 
-    private func configureItem() {
+    private func configureNavigationBar() {
         self.navigationController?.navigationBar.topItem?.title = "My Classes"
         self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: nil
         )
+    }
+    
+    private func setCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        self.collectionView.register(UINib(nibName: "MyClassesCollectionViewCell", bundle: Bundle.main), forCellWithReuseIdentifier: "myClassesCollectionViewCell")
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myClassesCollectionViewCell", for: indexPath as IndexPath) as! MyClassesCollectionViewCell
+        cell.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.layer.cornerRadius = 15
+        cell.layer.borderWidth = 1
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
 }
 
