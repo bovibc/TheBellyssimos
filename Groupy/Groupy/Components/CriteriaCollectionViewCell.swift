151032fa99b6fe49e0097164304476859bb2d444
carolquiterio <carolquiterio@yahoo.com.br>
2023-07-05 10:59:29 -0300

    Add criteria xib cell

---
+++ Groupy/Groupy/Components/CriteriaCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CriteriaCollectionViewCell.swift
+//  Groupy
+//
+//  Created by Carol Quiterio on 05/07/23.
+//
+
+import UIKit
+
+class CriteriaCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var Icon: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
