3043a99c004ebe59a11b4957e6db1068a5c4e92f
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2023-07-07 11:49:23 -0300

    Profile: init

--- Groupy/Groupy/Main/ProfileViewController.swift
+++ Groupy/Groupy/Main/ProfileViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    private let sectionNumber: Int = 2
+    private let declarationCellNumber: Int = 4
+    private let skillCellNumber: Int = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Placeholders
+        var image = UIImage(named: "opera")
+        image = resizeImage(image: image!, targetSize: CGSize(width: 100, height: 100))
+        profilePicture.image = image
+        profilePicture.layer.borderColor = UIColor(red:1, green:1, blue:1, alpha: 0).cgColor
+        profilePicture.layer.borderWidth = 1
+        profilePicture.layer.masksToBounds = false
+        profilePicture.layer.cornerRadius = profilePicture.frame.size.width / 2
+        profilePicture.clipsToBounds = true
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -20,5 +42,79 @@ class ProfileViewController: UIViewController {
     
     private func configureNavigationBar() {
         self.navigationController?.navigationBar.topItem?.title = "Profile"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+
+        view.backgroundColor = UIColor.systemGray6
+    }
+    
+    private func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+          newSize = CGSize(width: targetSize.width, height: targetSize.height)
+        } else {
+          newSize = CGSize(width: targetSize.width, height: targetSize.height)
+        }
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+}
+
+extension ProfileViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    // tamanho celula
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 140
+//    }
+    
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    // quantidade de celulas
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return declarationCellNumber
+        } else {
+            return skillCellNumber
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "SELF-DECLARATION"
+        } else {
+            return "SKILLS"
+        }
     }
 }
