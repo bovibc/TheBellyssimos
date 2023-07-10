4b6985f82f82eb1b7d4e217ee06fd1d541e535e8
lufazol <luanfazolin@gmail.com>
2023-07-10 09:31:43 -0300

    Create login screen

--- Groupy/Groupy/LoginFlow/LoginViewController.swift
+++ Groupy/Groupy/LoginFlow/LoginViewController.swift
@@ -12,7 +12,9 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.systemGray6
+
+        title = "Login"
     }
     
 
