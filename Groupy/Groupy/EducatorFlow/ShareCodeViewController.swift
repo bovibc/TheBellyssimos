4cf5ce0bfde09e02d1c029387c87376e68541037
lufazol <luanfazolin@gmail.com>
2023-07-11 11:47:45 -0300

    Connect share code screen to other screens

--- Groupy/Groupy/EducatorFlow/ShareCodeViewController.swift
+++ Groupy/Groupy/EducatorFlow/ShareCodeViewController.swift
@@ -12,10 +12,21 @@ class ShareCodeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = UIColor.systemGray6
+
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func maybeLatterTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
 
+        var presentingViewController = self.presentingViewController
+        while presentingViewController != nil {
+            presentingViewController?.dismiss(animated: true, completion: nil)
+            presentingViewController = presentingViewController?.presentingViewController
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
