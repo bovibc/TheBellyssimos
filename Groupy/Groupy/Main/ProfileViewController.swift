f03db209a37f4265171d3a83a6beaf1ec630f64c
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2023-07-11 09:12:24 -0300

    Profile Update

--- Groupy/Groupy/Main/ProfileViewController.swift
+++ Groupy/Groupy/Main/ProfileViewController.swift
@@ -10,25 +10,18 @@ import AVFoundation
 
 class ProfileViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var logOutButton: UIButton!
     
-    private let sectionNumber: Int = 2
-    private let declarations: [String] = ["MBTI", "Working style","Skin tone", "Gender","Enthnicity"]
-    private let skills: [String] = ["Swift", "Figma"]
-    private let parameters = [MockData().mbtiPersonality,MockData().wsPersonality,MockData().skinTone,MockData().gender,MockData().ethnicities]
-
+    private let textTable: [String] = ["Change passwod", "Change e-mail"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
         tableView.delegate = self
         tableView.dataSource = self
-        let nib1 = UINib(nibName: "PickerTableViewCell", bundle: nil)
-        let nib2 = UINib(nibName: "CheckTableViewCell", bundle: nil)
-        tableView.register(nib1, forCellReuseIdentifier: PickerTableViewCell.cellIdentifier)
-        tableView.register(nib2, forCellReuseIdentifier: CheckTableViewCell.cellIdentifier)
         configureImage()
     }
     
@@ -85,17 +78,6 @@ class ProfileViewController: UIViewController {
 extension ProfileViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Unselect the row, and instead, show the state with a checkmark.
-        tableView.deselectRow(at: indexPath, animated: false)
-        
-        guard let cell = tableView.cellForRow(at: indexPath) else { return }
-        
-        // Show a check mark next to packed items.
-        if cell.accessoryType == .checkmark {
-            cell.accessoryType = .none
-        } else {
-            cell.accessoryType = .checkmark
-        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -106,13 +88,9 @@ extension ProfileViewController: UITableViewDelegate {
 
 extension ProfileViewController: UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-
     // quantidade de celulas
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (section == 0 ? declarations.count : skills.count)
+        return textTable.count
     }
     
     private func roundingCornersCell(_ cell: UITableViewCell, _ indexPath: IndexPath){
@@ -148,32 +126,21 @@ extension ProfileViewController: UITableViewDataSource {
         }
     }
     
-    private func getPickerCell(_ indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PickerTableViewCell.cellIdentifier, for: indexPath) as! PickerTableViewCell
-        cell.setCell(myLabel: declarations[indexPath.row], myPicker: parameters[indexPath.row])
-        roundingCornersCell(cell, indexPath)
-        return cell
-    }
-    
-    private func getCheckCell(_ indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CheckTableViewCell.cellIdentifier, for: indexPath) as! CheckTableViewCell
-        
-        cell.setCell(myLabel: skills[indexPath.row])
+    private func getCell(_ indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath) as! ProfileTableViewCell
+        cell.labelDescription.text = textTable[indexPath.row]
         roundingCornersCell(cell, indexPath)
         return cell
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.section == 0 {
-            return getPickerCell(indexPath)
-        } else {
-            return getCheckCell(indexPath)
-        }
+        return getCell(indexPath)
         
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return (section == 0 ? "SELF-DECLARATION" : "SKILLS")
-    }
+            return "SETTINGS"
+        }
+    
 }
